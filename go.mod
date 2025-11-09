143056cad8e92a4eaa5514e67469020ad8af81ba
Alabi Fathiu <alabiabolore4@gmail.com>
2025-11-09 18:31:59 +0100

    second test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/aboloredev/puppyPlay
 
 go 1.25.4
+
+require github.com/AboloreDev/GoFarm v0.0.0-20251109163448-d468af91527e // indirect
--- puppyPlace.go
+++ puppyPlace.go
@@ -1,10 +1,20 @@
 package puppyplace
 
-
+import (
+	gofarm "github.com/AboloreDev/GoFarm"
+)
 func Cock() string {
 	return "kookoorookoo"
 }
 
 func Cocks() string {
 	return "Loud kookoorookoo"
+}
+
+func BigBark() string {
+	return gofarm.Sound(Cock())
+}
+
+func BigBarks() string {
+	return gofarm.Sound(Cocks())
 }
